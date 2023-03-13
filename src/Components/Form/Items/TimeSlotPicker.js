b827fba08c5096c5e906671e3563424bd81b29fd
vishesh102 <chaudhary.vishesh@athansys.com>
2023-03-13 12:41:44 +0530

    latest

--- src/Components/Form/Items/TimeSlotPicker.js
+++ src/Components/Form/Items/TimeSlotPicker.js
@@ -1,34 +1,13 @@
 import * as React from "react";
-import dayjs from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 import { Input, TextField } from "@mui/material";
+import { Label } from "@mui/icons-material";
 
 function TimeSlotPicker({ label, onChange, name }) {
   return (
-    // <LocalizationProvider dateAdapter={AdapterDayjs}>
-    //   <DemoContainer
-    //     components={[
-    //       "TimePicker",
-    //       "MobileTimePicker",
-    //       "DesktopTimePicker",
-    //       "StaticTimePicker",
-    //     ]}
-    //   >
-    //     <DemoItem label={label}>
-    //       <MobileTimePicker
-    //         // name={name}
-    //         // onChange={onChange}
-    //         defaultValue={dayjs("2022-04-17T15:30")}
-    //       />
-    //     </DemoItem>
-    //   </DemoContainer>
-    // </LocalizationProvider>
     <div>
-      <Input type="date-time" />
+      <label style={{ margin: " 0 4rem" }}>{label}</label>
+      <Input type="time" name={name} onChange={onChange} />
     </div>
   );
 }
